c5681675ef0b555bc3c453ceb492197095f6b793
Kyle Shumway <root1@Kyles-MacBook-Pro.local>
2017-02-22 16:22:24 -0700

    Removed the content from views and set it in public that way we can use it with angular

--- meetups/public/javascripts/angularApp.js
+++ meetups/public/javascripts/angularApp.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('myApp', ['ngRoute']);
+ var myApp = angular.module('myApp', ['ngRoute']);
 myApp.config(['$routeProvider', function($routeProvider){
   $routeProvider.when('/home', {
     templateUrl: 'partials/home.ejs'
